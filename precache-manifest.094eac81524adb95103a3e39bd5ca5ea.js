d521e2ad1e2936f3f42065a68b3f01f8ff195c78
xuelongmu <xuelongmu@wacai.com>
2018-11-11 22:18:06 +0800

    添加测试网站

---
+++ precache-manifest.094eac81524adb95103a3e39bd5ca5ea.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = [
+  {
+    "revision": "40c77453dfd0a6d0f08fe7ccf030b81e",
+    "url": "/static/media/textile.40c77453.jpeg"
+  },
+  {
+    "revision": "167d4c3e268831d48507",
+    "url": "/static/css/main.113395ff.chunk.css"
+  },
+  {
+    "revision": "d0c0b567b941ce32d7b9297c3e978488",
+    "url": "/static/media/big_logo.d0c0b567.png"
+  },
+  {
+    "revision": "539c20b0877fc9a93b0a",
+    "url": "/static/js/1.539c20b0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "94313afad809e8951bb94e405eaf109f",
+    "url": "/static/media/shenqun.94313afa.png"
+  },
+  {
+    "revision": "146785bf0d5a929dd8882774e9be6d57",
+    "url": "/static/media/yangluwei.146785bf.png"
+  },
+  {
+    "revision": "440db49f18db24b31dc9e7c70c420c80",
+    "url": "/static/media/technology_count.440db49f.jpg"
+  },
+  {
+    "revision": "be71ae68f0f071c280edc381b21b76d8",
+    "url": "/static/media/home_text.be71ae68.jpeg"
+  },
+  {
+    "revision": "63f1c52e6c853e4d65808bd233df6204",
+    "url": "/static/media/make_shoe.63f1c52e.jpeg"
+  },
+  {
+    "revision": "b96310c15cd94000f0065673f358edb9",
+    "url": "/static/media/showmaker.b96310c1.jpg"
+  },
+  {
+    "revision": "458c43c1d86c15f83852119fa2e01b38",
+    "url": "/static/media/bicycle.458c43c1.jpeg"
+  },
+  {
+    "revision": "f1bc1d635c94ccdfbf80eb9e52ad6352",
+    "url": "/static/media/forestry.f1bc1d63.jpeg"
+  },
+  {
+    "revision": "d9e210b8812532fae89a8b94505d112e",
+    "url": "/static/media/livestock.d9e210b8.jpeg"
+  },
+  {
+    "revision": "c0d6be3c90be76367fd0455c17de7776",
+    "url": "/static/media/metallurgy.c0d6be3c.jpeg"
+  },
+  {
+    "revision": "4f3236a7cd9c44724bc069146aadc8a1",
+    "url": "/static/media/petrochemical.4f3236a7.jpeg"
+  },
+  {
+    "revision": "325cab75773f618ed145ebabb41987c2",
+    "url": "/static/media/pharmacy.325cab75.jpeg"
+  },
+  {
+    "revision": "167d4c3e268831d48507",
+    "url": "/static/js/main.167d4c3e.chunk.js"
+  },
+  {
+    "revision": "3db321c62c8cf3321cf3d83fa174be57",
+    "url": "/static/media/daodianjiao.3db321c6.jpeg"
+  },
+  {
+    "revision": "0a3472df8549e7053edbd1102f0c64d4",
+    "url": "/static/media/dihaodianguanlishebei.0a3472df.jpeg"
+  },
+  {
+    "revision": "d570c71e4c72e65455e28eb218cb53e8",
+    "url": "/static/media/fanyiruanjian.d570c71e.jpg"
+  },
+  {
+    "revision": "b2eb6cf570b1b56424826b8040ead071",
+    "url": "/static/media/fengguanghubureshuiqi.b2eb6cf5.jpeg"
+  },
+  {
+    "revision": "888b743de66a8496ddbda0dc1a692427",
+    "url": "/static/media/guntongzhazhiji.888b743d.jpeg"
+  },
+  {
+    "revision": "3d24fe61cf57f881db166f664b2dadb2",
+    "url": "/static/media/kediaoshipeitou.3d24fe61.jpeg"
+  },
+  {
+    "revision": "bb7d5ed658025437c3c41370a4d07e9b",
+    "url": "/static/media/kekongpenzaixitong.bb7d5ed6.jpeg"
+  },
+  {
+    "revision": "bc12f6ddec8f9ed84919e41b23608491",
+    "url": "/static/media/lvdaiyunshujiqiren.bc12f6dd.jpeg"
+  },
+  {
+    "revision": "57bc650ff74963b72842aab60122ef4a",
+    "url": "/static/media/shangkouyuhe.57bc650f.jpeg"
+  },
+  {
+    "revision": "46efacc9d9dcf3cfd40dde3558abf87f",
+    "url": "/static/media/shengpingzhang.46efacc9.jpeg"
+  },
+  {
+    "revision": "594a860c4670190c4d18cff018a997b8",
+    "url": "/static/media/shuilujiqiren.594a860c.jpeg"
+  },
+  {
+    "revision": "65114c0491dd486d428e1a3e7c35667c",
+    "url": "/static/media/taiyangnengchuan.65114c04.jpeg"
+  },
+  {
+    "revision": "b3910c532faaa64b6c2f6c01963d9914",
+    "url": "/static/media/taiyangnengdianchi.b3910c53.gif"
+  },
+  {
+    "revision": "21c2b396b4f8f1029eb9825bc26ab59e",
+    "url": "/static/media/wurenyunshuji.21c2b396.jpeg"
+  },
+  {
+    "revision": "c041356fa957e6d8439660628f9bda6c",
+    "url": "/static/media/wuxianjiguangchongdian.c041356f.jpeg"
+  },
+  {
+    "revision": "6d23311b77bd21e7846972b5a8ada88a",
+    "url": "/static/media/cuihuaji.6d23311b.jpg"
+  },
+  {
+    "revision": "8cf78f78d203ce7abb44770356e013ce",
+    "url": "/static/media/hehechouhe.8cf78f78.jpg"
+  },
+  {
+    "revision": "0025a97094d9f651a3ff8e1ec3ef7f71",
+    "url": "/static/media/hunhechedongli.0025a970.jpg"
+  },
+  {
+    "revision": "4bb45e6787d1c360186404ccab54bdf6",
+    "url": "/static/media/jiaquanpuzhuo.4bb45e67.jpg"
+  },
+  {
+    "revision": "1f5ae80460956d20bac8cf17a10f9b91",
+    "url": "/static/media/miejunchuwei.1f5ae804.jpg"
+  },
+  {
+    "revision": "06aaa46d8634be27ada85dcc671db5e5",
+    "url": "/static/media/quanzidongbaozhuang.06aaa46d.jpg"
+  },
+  {
+    "revision": "7813dc812105bb3aa916c15fa216085e",
+    "url": "/static/media/shengwuxiufu.7813dc81.jpg"
+  },
+  {
+    "revision": "054a761bc0e76aba5732aa68194d576e",
+    "url": "/static/media/shuixitxiufu.054a761b.jpg"
+  },
+  {
+    "revision": "84c48e0b8a708a7543366189781dd461",
+    "url": "/static/media/suanxingmuyu.84c48e0b.jpg"
+  },
+  {
+    "revision": "e3ebb5f50025e449ba0ace1746019bc1",
+    "url": "/static/media/wumaizhiwu.e3ebb5f5.jpg"
+  },
+  {
+    "revision": "00898cead0293141bcb7b60f5fafc9ba",
+    "url": "/static/media/xianweisu.00898cea.jpg"
+  },
+  {
+    "revision": "9d049700d88bb6e253eca63efaf458a0",
+    "url": "/static/media/xieditujiao.9d049700.jpg"
+  },
+  {
+    "revision": "6b757e407248b8c68dce4f142c9253b8",
+    "url": "/static/media/yingerkechuandai.6b757e40.jpg"
+  },
+  {
+    "revision": "3cba6f5c4d09cca0a4eb6cafba897cc5",
+    "url": "/static/media/yunichuli.3cba6f5c.jpg"
+  },
+  {
+    "revision": "f0d23500bf2231b7df4c1c1261aa5b4e",
+    "url": "/static/media/zhinengmensuo.f0d23500.jpg"
+  },
+  {
+    "revision": "539c20b0877fc9a93b0a",
+    "url": "/static/css/1.438352e3.chunk.css"
+  },
+  {
+    "revision": "275860fc94addace4720b461a9114da3",
+    "url": "/index.html"
+  }
+];
